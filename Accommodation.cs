cd5bf5b84ed41d2561cb6bf3aadc3f6ef490e8de
Mjabulelwa <mjabulelwawelford@gmail.com>
2025-07-11 01:46:41 +0200

    Add files via upload

---
+++ Accommodation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LadysmithProperties.Model
+{
+    public class Accommodation
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int AccommodationId { get; set; }
+        public string AccommodationName { get; set; }
+        public string AccommodationDescription { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal AccommodationPrice { get; set; }
+
+        public string ImageUrls { get; set; }
+    }
+}
