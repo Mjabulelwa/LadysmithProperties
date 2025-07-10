6a4273ac1fdde397ff2c8563a6770d5922a7f7a7
Mjabulelwa <mjabulelwawelford@gmail.com>
2025-07-11 01:41:42 +0200

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LadysmithProperties.Data;
+using LadysmithProperties.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LadysmithProperties.Controllers
+{
+  
+    public class HomeController : Controller
+    {
+      
+        public HomeController()
+        {
+           
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+      
+    }
+}
