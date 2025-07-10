4dd8935b870cdf322c8c435f7e4af9340ce2b085
Mjabulelwa <mjabulelwawelford@gmail.com>
2025-07-11 01:43:09 +0200

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using LadysmithProperties.Model;
+
+namespace LadysmithProperties.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Accommodation> Accommodations { get; set; }
+        public DbSet<Renting> Rentings { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Accommodation>().ToTable("Accommodation");
+            modelBuilder.Entity<Renting>().ToTable("Renting");
+            modelBuilder.Entity<Tenant>().ToTable("Tenant");
+        }
+    }
+}
