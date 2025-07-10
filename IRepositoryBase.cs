7a7e25d42d69df3e27088155777a2aa06c17be5d
Mjabulelwa <mjabulelwawelford@gmail.com>
2025-07-11 01:44:38 +0200

    Add files via upload

---
+++ IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+namespace LadysmithProperties.Data
+{
+    public interface IRepositoryBase<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void create(T entity);
+        void Update(T entity);
+        void delete(T entity);
+
+    }
+}
