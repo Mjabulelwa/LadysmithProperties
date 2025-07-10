52f20e6406ab62ee22e9fefe664f4d13b6245208
Mjabulelwa <mjabulelwawelford@gmail.com>
2025-07-11 01:42:34 +0200

    Add files via upload

---
+++ AccommodationRepository.cs
@@ -0,0 +1,26 @@
+﻿using LadysmithProperties.Model;
+
+namespace LadysmithProperties.Data
+{
+    public class AccommodationRepository : IAccommodationRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AccommodationRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<Accommodation> Accommodations
+        {
+            get
+            {
+                return _appDbContext.Accommodations;
+            }
+        }
+
+        public Accommodation GetAccommodationById(int id)
+        {
+            return _appDbContext.Accommodations.FirstOrDefault(a=> a.AccommodationId == id);
+        }
+    }
+}
