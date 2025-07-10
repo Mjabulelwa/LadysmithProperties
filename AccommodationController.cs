dfed1f046fb5f32a5ab6a6fe98381a0db025badf
Mjabulelwa <mjabulelwawelford@gmail.com>
2025-07-11 01:41:15 +0200

    Add files via upload

---
+++ AccommodationController.cs
@@ -0,0 +1,18 @@
+﻿using LadysmithProperties.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LadysmithProperties.Controllers
+{
+    public class AccommodationController:Controller
+    {
+        private readonly IAccommodationRepository _accommodationRepository;
+        public AccommodationController(IAccommodationRepository accommodationRepository) 
+        {
+            _accommodationRepository = accommodationRepository;
+        }
+        public IActionResult Accommodations()
+        {
+            return View(_accommodationRepository.Accommodations);
+        }
+    }
+}
